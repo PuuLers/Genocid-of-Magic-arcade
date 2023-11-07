e7431f74c23627bcfe48b1d0a1262df854c4cfcd
cans1z <canss1zzz@gmail.com>
2023-11-08 01:21:16 +0300

    half of fix

--- Genocid of Magic Arcade/Assets/Scripts/Weapons.cs
+++ Genocid of Magic Arcade/Assets/Scripts/Weapons.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class Weapons : MonoBehaviour
 {
+    [SerializeField] private float _speed;
+    [SerializeField] private Transform _marker;
+    [SerializeField] private Transform _weapon;
+    public bool rot_to_angle;
+    public int offset = 90;
 
+    public void Update()
+    {
+        Rotate();
+    }
 
-    //public void TransformSend()
-    //{
-    //    if (Input.GetMouseButtonDown(0))
-    //    {
-    //        gameObject.transform.rotation = new Vector ;
-    //    }
-       
-    //}
-
-
-
+    public void Rotate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (rot_to_angle)
+            {
+                _weapon.rotation = Quaternion.Lerp(_weapon.rotation, _marker.rotation, _speed * Time.deltaTime);
+            }
+        }
+    }
 }
